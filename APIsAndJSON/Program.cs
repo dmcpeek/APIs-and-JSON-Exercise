e7a6cf92571f25735e1fb8c734c1eff8e37fd988
Doug McPeek <dmcpeek@outlook.com>
2023-05-13 11:32:54 -0600

    Add files via upload

---
+++ APIsAndJSON/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                RonVSKanyeAPI.RonS();
+
+                RonVSKanyeAPI.Kanye();
+
+                Console.WriteLine("----------");
+
+            }
+            Console.WriteLine("Press the any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIsAndJSON
+{
+    public class RonVSKanyeAPI
+    {
+        #region Ron
+        public static void RonS()
+        {
+            var client = new HttpClient();
+            
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes"; // Endpoint
+           
+            string ronResponse = client.GetStringAsync(ronURL).Result; // Send GET request to the API
+            
+            var ronObject = JArray.Parse(ronResponse); // Parse response into JArray
+            
+            
+            Console.WriteLine($"With barely conatined disdain, Ron muttered, \"{ronObject[0]}\""); // [0] Removes brackets when response is displayed
+        }
+        #endregion
+
+        #region Kanye
+        public static void Kanye()
+        {
+            var client = new HttpClient();
+
+            var kayneURL = "https://api.kanye.rest/"; // Endpoint
+
+            string kayneResponse = client.GetStringAsync(kayneURL).Result; // Send GET request to the API
+            
+            var kayneObject = JObject.Parse(kayneResponse); // Parse response into JObject
+            
+            Console.WriteLine($"Kanya inanely stated, \"{kayneObject["quote"]}\""); // Value grabs the "joke" text 
+        }
+
+        #endregion
+    }
+}
